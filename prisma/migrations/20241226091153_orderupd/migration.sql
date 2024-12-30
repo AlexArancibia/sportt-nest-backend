6926f0d277ce81154d3a02b65f6eaee360602878
AlexArancibia <alexanderarancibia37@gmail.com>
2024-12-29 23:58:51 -0500

    first version

--- prisma/migrations/20241226091153_orderupd/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "OrderItems" ADD COLUMN     "variantId" TEXT;
-
--- AddForeignKey
-ALTER TABLE "OrderItems" ADD CONSTRAINT "OrderItems_variantId_fkey" FOREIGN KEY ("variantId") REFERENCES "Variant"("id") ON DELETE SET NULL ON UPDATE CASCADE;
